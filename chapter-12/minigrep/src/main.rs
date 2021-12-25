ef1aae3b54eaf1575a2fa1b527b90abef7199ebb
Ricard Mallafre <rikitzzz@gmail.com>
2021-12-25 13:06:04 +0100

    refactor: using Config struct
    The Config struct is responsible for retrieving the correspoding command
    line arguments and handling errors without panicking.

--- chapter-12/minigrep/src/main.rs
+++ chapter-12/minigrep/src/main.rs
@@ -1,22 +1,38 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
