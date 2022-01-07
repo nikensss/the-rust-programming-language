46db65f6dd90e35b825ff4c20717fc42ce2aa6e1
Ricard Mallafre <rikitzzz@gmail.com>
2022-01-07 14:11:13 +0100

    feat: cacher runs for different inputs

--- chapter-13/src/main.rs
+++ chapter-13/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 
@@ -13,7 +14,7 @@ where
     T: Fn(u32) -> u32,
 {
     calculation: T,
-    value: Option<u32>,
+    values: HashMap<u32, u32>,
 }
 
 impl<T> Cacher<T>
@@ -23,16 +24,17 @@ where
     fn new(calculation: T) -> Cacher<T> {
         Cacher {
             calculation,
-            value: None,
+            values: HashMap::new(),
         }
     }
 
     fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
+        let value = self.values.get(&arg);
+        match value {
+            Some(v) => *v,
             None => {
                 let v = (self.calculation)(arg);
-                self.value = Some(v);
+                self.values.insert(arg, v);
                 v
             }
         }
@@ -59,4 +61,16 @@ fn generate_workout(intensity: u32, random_number: u32) {
             )
         }
     }
+}
+
+#[test]
+fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(v1, 1);
+    assert_eq!(v2, 2);
+    assert_ne!(v1, v2);
 }
