cc3cce4d1bb34459bb6a965403d10fbf55368768
Ricard Mallafre <rikitzzz@gmail.com>
2022-02-13 12:09:52 +0100

    feat: extra checks for approval requirements
    A post can be set to require more or less approvals.

--- chapter-17-blog/blog/src/state_with_types.rs
+++ chapter-17-blog/blog/src/state_with_types.rs
@@ -53,6 +53,10 @@ impl PendingReviewPost {
         self.approvals == self.required_approvals
     }
 
+    pub fn get_approvals(&self) -> u32 {
+        self.approvals
+    }
+
     pub fn get_post(self) -> Option<Post> {
         if self.approvals == self.required_approvals {
             return Some(Post {
@@ -61,16 +65,28 @@ impl PendingReviewPost {
         }
         None
     }
+
+    pub fn get_required_approvals(&self) -> u32 {
+        self.required_approvals
+    }
+
+    pub fn set_required_approvals(&mut self, required_approvals: u32) {
+        self.required_approvals = required_approvals
+    }
 }
 
 pub fn pseudo_main() {
     let mut post = Post::new();
     post.add_text("I ate a salad for lunch today");
     let mut post = post.request_review();
+    post.set_required_approvals(4);
+    assert_eq!(4, post.get_required_approvals());
+
     while !post.is_approved() {
         post.approve();
     }
 
+    assert_eq!(post.get_approvals(), post.get_required_approvals());
     if let Some(p) = post.get_post() {
         assert_eq!("I ate a salad for lunch today", p.content());
     } else {
