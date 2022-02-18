608d2c9cf18e0b5819103eb1dcab62f79760e0b9
Ricard Mallafre <rikitzzz@gmail.com>
2022-02-18 18:36:53 +0100

    feat: created worker struct
    The Worker struct is used by the ThreadPool to execute the jobs it
    receives throw the `ThreadPool::execute` method.

--- chapter-20/hello/src/lib.rs
+++ chapter-20/hello/src/lib.rs
@@ -1,13 +1,45 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The `size` is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    // TODO: implement this method returning a Result type
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool {}
+        assert!(size > 0);
+
+        let mut workers = Vec::with_capacity(size);
+
+        for i in 0..size {
+            workers.push(Worker::new(i));
+        }
+
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+        Worker { id, thread }
     }
 }
