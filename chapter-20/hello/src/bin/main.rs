c69dc8532a565542d2595073445b779765930eba
Ricard Mallafre <rikitzzz@gmail.com>
2022-02-18 18:17:14 +0100

    feat: adding ThreadPool
    The code compiles but it does nothing.

--- chapter-20/hello/src/bin/main.rs
+++ chapter-20/hello/src/bin/main.rs
@@ -1,3 +1,4 @@
+use hello::ThreadPool;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
@@ -7,12 +8,16 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
     println!("Listening for requests on http://localhost:7878...");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
